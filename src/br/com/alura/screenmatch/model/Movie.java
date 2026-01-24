d01ce79ff4e7d303bd00d46fa90bd223a7ae36b5
Sara Santos <sarasantosdev@outlook.com>
2026-01-24 00:53:37 -0300

    feat: modifica acesso e adiciona setters

--- src/br/com/alura/screenmatch/model/Movie.java
+++ src/br/com/alura/screenmatch/model/Movie.java
@@ -4,16 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Movie {
-    String name;
-    int year;
-    int duration;
-    boolean included;
+    private String name;
+    private int year;
+    private int duration;
+    private boolean included;
     private List<Integer> ratings = new ArrayList<Integer>();
 
     public Movie(String name, int year, int duration, boolean included){
         this.name = name;
         this.year = year;
         this.duration = duration;
+        this.included = included;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setIncluded(boolean included) {
         this.included = included;
     }
 
