f44de1ecb9369a018aa0e0f9d8646a94892553ce
Sara Santos <sarasantosdev@outlook.com>
2026-01-25 21:26:36 -0300

    feat: sobrescreve os métodos de duração e ficha técnica

--- src/br/com/alura/screenmatch/model/Serie.java
+++ src/br/com/alura/screenmatch/model/Serie.java
@@ -10,5 +10,48 @@ public class Serie extends Title{
         super(name, year, duration, included);
     }
 
+    public int getSeason() {
+        return season;
+    }
 
+    public void setSeason(int season) {
+        this.season = season;
+    }
+
+    public int getEpisode() {
+        return episode;
+    }
+
+    public void setEpisode(int episode) {
+        this.episode = episode;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getMinutesEpisode() {
+        return minutesEpisode;
+    }
+
+    public void setMinutesEpisode(int minutesEpisode) {
+        this.minutesEpisode = minutesEpisode;
+    }
+
+    @Override
+    public int getDuration() {
+        return season * episode * minutesEpisode;
+    }
+
+    @Override
+    public void showTechnicSlip() {
+        super.showTechnicSlip();
+        System.out.println("Temporadas: " + season);
+        System.out.println("Episódios: " + episode);
+        System.out.println("Minutos por episódio: " + minutesEpisode);
+    }
 }
