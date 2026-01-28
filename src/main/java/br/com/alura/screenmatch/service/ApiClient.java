6c4c633f0d8eb3bb8278e823fab622e6f0914fe7
Sara Santos <sarasantosdev@outlook.com>
2026-01-28 15:29:12 -0300

    feat: adiciona o uso do Gson para serializar os dados

---
+++ src/main/java/br/com/alura/screenmatch/service/ApiClient.java
@@ -0,0 +1,41 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.model.Movie;
+import br.com.alura.screenmatch.model.Title;
+import br.com.alura.screenmatch.model.TitleOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class ApiClient {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o que deseja buscar: ");
+        String search = input.nextLine();
+
+        String url = "https://www.omdbapi.com/?t="+search+"&apikey=12a45813";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+
+        TitleOmdb titleOmdb = gson.fromJson(json, TitleOmdb.class);
+        System.out.println(titleOmdb);
+    }
+}
