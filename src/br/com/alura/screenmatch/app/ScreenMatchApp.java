b12acfa9b0988250bd4e69eb46557a685b3115a6
Sara Santos <sarasantosdev@outlook.com>
2026-01-27 22:55:18 -0300

    feat: ordena a lista de titulos

--- src/br/com/alura/screenmatch/app/ScreenMatchApp.java
+++ src/br/com/alura/screenmatch/app/ScreenMatchApp.java
@@ -2,29 +2,24 @@ package br.com.alura.screenmatch.app;
 
 import br.com.alura.screenmatch.model.Movie;
 import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.Title;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScreenMatchApp {
     public static void main(String[] args) {
-        Movie movie = new Movie("O Poderoso Chefão", 1972);
-
-        movie.setDirector("Francis Ford Coppola");
-        movie.setDurationMinutes(177);
-        movie.setIncluded(true);
-
-        System.out.println("Ficha técnica do filme");
-
-        movie.showTechnicSlip();
+        Movie movie = new Movie("The GodFather", 1972);
 
         Serie serie = new Serie("Lost", 2004);
 
-        System.out.println("\nFicha técnica da série");
+        List<Title> titles = new ArrayList<>();
 
-        serie.setSeason(6);
-        serie.setEpisode(121);
-        serie.setMinutesEpisode(40);
-        serie.setDurationMinutes(40);
+        titles.add(movie);
+        titles.add(serie);
 
-        serie.showTechnicSlip();
+        titles.sort(Title::compareTo);
 
+        System.out.println(titles);
     }
 }
