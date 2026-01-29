a771cf4e45b91b7a8ca8d32125b8a5cb17557078
Sara Santos <sarasantosdev@outlook.com>
2026-01-29 01:19:15 -0300

    feat: trata exceção IllegalArgumentException

--- src/main/java/br/com/alura/screenmatch/service/ApiClient.java
+++ src/main/java/br/com/alura/screenmatch/service/ApiClient.java
@@ -8,9 +8,11 @@ import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class ApiClient {
@@ -19,31 +21,23 @@ public class ApiClient {
         System.out.println("Digite o que deseja buscar: ");
         String search = input.nextLine();
 
-        String url = "https://www.omdbapi.com/?t="+search+"&apikey=12a45813";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-
-        TitleOmdb titleOmdb = gson.fromJson(json, TitleOmdb.class);
-
         try{
-            Movie movie = new Movie(titleOmdb);
-            System.out.println(movie);
-        }catch (Exception e){
-            System.out.println("Error: " + e.getMessage());
-        }
+            String encodeSearch = URLEncoder.encode(search, StandardCharsets.UTF_8);
+            String url = "https://www.omdbapi.com/?t="+encodeSearch+"&apikey=12a45813";
 
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
+            TitleOmdb titleOmdb = gson.fromJson(json, TitleOmdb.class);
+            System.out.println(titleOmdb);
+        }catch (IllegalArgumentException e){
+            System.out.println("Erro na busca: verifique a url (endereço)");
+        }finally {
+            input.close();
+        }
     }
 }
