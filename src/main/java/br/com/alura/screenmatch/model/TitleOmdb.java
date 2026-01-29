e0d1ac5d8b3ea7e43261cbe1348f5e2d3ec79fe5
Sara Santos <sarasantosdev@outlook.com>
2026-01-28 23:55:43 -0300

    feat: adiciona método para extrair apenas os 4 digitos do ano

--- src/main/java/br/com/alura/screenmatch/model/TitleOmdb.java
+++ src/main/java/br/com/alura/screenmatch/model/TitleOmdb.java
@@ -1,7 +1,8 @@
 package br.com.alura.screenmatch.model;
 
-public record TitleOmdb(
-        String title,
-        String year,
-        String runtime
-) { }
+public record TitleOmdb(String title, String year, String runtime) {
+    public String getOnlyYear(){
+        return year.substring(0,4);
+    }
+
+}
